70168c91e3aabb0735fc8680217f8d8c2c58d7ec
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-08-23 03:13:41 +0300

    Add files via upload

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -4,27 +4,31 @@
 FragTrap::FragTrap( void )
 	: ClapTrap()
 {
-	std::cout << F_R_CYAN << "Default constructor started. FragTrap " << F_R_PRPL << getName() << F_R_CYAN << " constructed." << RESET << std::endl;
+	const char	*className = typeid(*this).name();
+	std::cout << F_R_CYAN << "Default constructor started. " << ++className << " " << F_R_PRPL << getName() << F_R_CYAN << " constructed." << RESET << std::endl;
 }
 
 //param constructor
 FragTrap::FragTrap( std::string name )
 	: ClapTrap( name, 100, 100, 30 )
 {
-	std::cout << F_R_CYAN << "Parametric constructor started. FragTrap " << F_R_PRPL << getName() << F_R_CYAN << " constructed." << RESET << std::endl;
+	const char	*className = typeid(*this).name();
+	std::cout << F_R_CYAN << "Parametric constructor started. " << ++className << " " << F_R_PRPL << getName() << F_R_CYAN << " constructed." << RESET << std::endl;
 }
 
 //copy constructor
 FragTrap::FragTrap( const FragTrap& other )
 {
 	*this = other;
-	std::cout << F_R_CYAN << "Copy constructor started. FragTrap " << F_R_PRPL << getName() << F_R_CYAN << " constructed." << RESET << std::endl;
+	const char	*className = typeid(*this).name();
+	std::cout << F_R_CYAN << "Copy constructor started. " << ++className << " " << F_R_PRPL << getName() << F_R_CYAN << " constructed." << RESET << std::endl;
 }
 
 //destructor
 FragTrap::~FragTrap( void )
 {
-	std::cout << F_R_YLLW << "Destructor started. FragTrap " << F_R_PRPL << getName() << F_R_YLLW << " destructed." << RESET << std::endl;
+	const char	*className = typeid(*this).name();
+	std::cout << F_R_YLLW << "Destructor started. " << ++className << " " << F_R_PRPL << getName() << F_R_YLLW << " destructed." << RESET << std::endl;
 }
 
 //[=] operator overload
@@ -43,5 +47,6 @@ FragTrap& FragTrap::operator= ( const FragTrap& other )
 //m-methods
 void	FragTrap::highFivesGuys( void )
 {
-	std::cout << F_R_GRN << "FragTrap " << F_R_PRPL << getName() << F_R_GRN << " is requesting for high five to him." << RESET << std::endl;
+	const char	*className = typeid(*this).name();
+	std::cout << F_R_GRN << ++className << " " << F_R_PRPL << getName() << F_R_GRN << " is requesting for high five to him." << RESET << std::endl;
 }
