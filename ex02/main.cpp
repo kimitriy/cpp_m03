87d33d4073f894f32dfda27bb76cb826212a0be8
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-08-24 04:34:46 +0300

    Add files via upload

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,28 +2,52 @@
 
 int		main()
 {
-	FragTrap incognito;
-	FragTrap joe( "Joe" );
-	FragTrap joes_bro( joe );
-	FragTrap bob;
+	// ClapTrap a;
+	// a.attack("Jack");
+	// a.takeDamage(5);
+	// a.beRepaired(5);
+	// a.setName("Mini");
+	// a.attack("Jack");
+	// a.takeDamage(5);
+	// a.beRepaired(5);
 
-	bob = FragTrap( "Bob" );
-	incognito = bob;
+	// ClapTrap b("one");
+	// b.attack("Robin");
+	// b.takeDamage(10);
+	// b.beRepaired(10);
 
-	incognito.status();
-	joe.status();
-	joes_bro.status();
-	bob.status();
+	// ClapTrap c(b);
+	// c.attack("Derek");
+	// c.takeDamage(10);
+	// c.beRepaired(10);
 
-	joe.attack( bob.getName() );
-	bob.takeDamage( joe.getHitPoints() );
-	joes_bro.attack ( bob.getName() );
-	bob.takeDamage( joe.getHitPoints() );
-	bob.beRepaired( 2 );
+
+	FragTrap g;
+	g.attack("Miu");
+	g.takeDamage(5);
+	g.beRepaired(5);
+	g.highFivesGuys();
 	
-	incognito.status();
-	bob.status();
-	bob.highFivesGuys();
+	FragTrap h("Mau");
+	h.attack("Jack");
+	h.takeDamage(5);
+	h.beRepaired(5);
+	h.setName("lalala");
+	h.highFivesGuys();
+	
+	FragTrap i(h);
+	i.setName("Minimiu");
+	i.attack("Jack");
+	i.takeDamage(15);
+	i.beRepaired(45);
+	i.highFivesGuys();
+
+	FragTrap *z = new FragTrap("new_lala");
+	z->attack("Jack");
+	z->takeDamage(15);
+	z->beRepaired(45);
+	z->highFivesGuys();
+	delete z;
 
 	return ( 0 );
 }
