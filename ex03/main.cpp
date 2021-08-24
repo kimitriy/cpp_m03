ba8f704bef7fc528a28678c9d559104facc3135a
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-08-24 04:35:07 +0300

    Add files via upload

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -2,40 +2,102 @@
 
 int		main()
 {
-	DiamondTrap incognito;
-	DiamondTrap bob;
-	bob = DiamondTrap( "Bob" );
-	incognito = bob;
+	ClapTrap a;
+	a.attack("Jack");
+	a.takeDamage(5);
+	a.beRepaired(5);
+	a.setName("Mini");
+	a.attack("Jack");
+	a.takeDamage(5);
+	a.beRepaired(5);
+
+	ClapTrap b("one");
+	b.attack("Robin");
+	b.takeDamage(10);
+	b.beRepaired(10);
+
+	ClapTrap c(b);
+	c.attack("Derek");
+	c.takeDamage(10);
+	c.beRepaired(10);
 
 
-	FragTrap joe( "Joe" );
-	FragTrap joes_bro( joe );
-
-
-	incognito.status();
-	joe.status();
-	joes_bro.status();
-	bob.status();
-
-	// joe.attack( bob.getName() );
-	// bob.takeDamage( joe.getHitPoints() );
-	// joes_bro.attack ( bob.getName() );
-	// bob.takeDamage( joe.getHitPoints() );
-	// bob.beRepaired( 2 );
+	// ScavTrap d;
+	// d.attack("Lulu");
+	// d.takeDamage(5);
+	// d.beRepaired(5);
 	
-	// incognito.status();
-	// bob.status();
+	// ScavTrap e("Lilu");
+	// e.attack("Jack");
+	// e.takeDamage(5);
+	// e.beRepaired(5);
+	// e.setName("lalala");
+	// e.guardGate();
+	
+	// ScavTrap f(e);
+	// f.setName("minimini");
+	// f.attack("Jack");
+	// f.takeDamage(15);
+	// f.beRepaired(45);
+	// f.guardGate();
 
-	// bob.guardGate();
 
-	// ClapTrap joe( "Joe" );
-	// joe.attack( "Bob" );
+	// FragTrap g;
+	// g.attack("Miu");
+	// g.takeDamage(5);
+	// g.beRepaired(5);
+	// g.highFivesGuys();
+	
+	// FragTrap h("Mau");
+	// h.attack("Jack");
+	// h.takeDamage(5);
+	// h.beRepaired(5);
+	// h.setName("lalala");
+	// h.highFivesGuys();
+	
+	// FragTrap i(h);
+	// i.setName("Minimiu");
+	// i.attack("Jack");
+	// i.takeDamage(15);
+	// i.beRepaired(45);
+	// i.highFivesGuys();
 
-	// ScavTrap bob( "Bob" );
-	// bob.attack( joe.getName() );
-	// joe.whoAmI();
-	// joe.guardGate();
-	// joe.highFivesGuys();
+	// FragTrap *z = new FragTrap("new_lala");
+	// z->attack("Jack");
+	// z->takeDamage(15);
+	// z->beRepaired(45);
+	// z->highFivesGuys();
+	// delete z;
+
+
+	// DiamondTrap g;
+	// g.takeDamage(5);
+	// g.attack("Jack");
+	// g.beRepaired(5);
+	// g.highFivesGuys();
+	// g.whoAmI();
+
+	// DiamondTrap l("Black");
+	// l.attack("Jack");
+	// l.takeDamage(5);
+	// l.beRepaired(5);
+	// l.highFivesGuys();
+	// l.whoAmI();
+
+	// FragTrap m(l);
+	// m.attack("Jack");
+	// m.takeDamage(15);
+	// m.beRepaired(45);
+	// m.highFivesGuys();
+	// m.whoAmI();
+
+	// DiamondTrap *z = new DiamondTrap("new_lala");
+	// z->attack("Jack");
+	// z->takeDamage(15);
+	// z->beRepaired(45);
+	// z->highFivesGuys();
+	// z->whoAmI();
+	// delete z;
 
 	return ( 0 );
 }
